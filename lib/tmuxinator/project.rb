e923363499f3ddde47b4f532cbdadf0b4bb9a41f
Florent Vilmart <florent@flovilmart.com>
2018-09-16 14:37:08 -0400

    Adds support for tmuxinator append

--- lib/tmuxinator/project.rb
+++ lib/tmuxinator/project.rb
@@ -92,6 +92,7 @@ module Tmuxinator
 
       @force_attach = options[:force_attach]
       @force_detach = options[:force_detach]
+      @current_session = options[:current_session]
 
       raise "Cannot force_attach and force_detach at the same time" \
         if @force_attach && @force_detach
@@ -112,6 +113,10 @@ module Tmuxinator
       Erubis::Eruby.new(content).result(bndg)
     end
 
+    def in_current_session
+      @current_session ? @current_session : yaml["current_session"]
+    end
+
     def windows
       windows_yml = yaml["tabs"] || yaml["windows"]
 
@@ -126,6 +131,12 @@ module Tmuxinator
     end
 
     def name
+      name = custom_name || yaml["project_name"] || yaml["name"]
+      name = blank?(name) ? nil : name.to_s.shellescape
+      in_current_session ? "" : name
+    end
+
+    def full_name
       name = custom_name || yaml["project_name"] || yaml["name"]
       blank?(name) ? nil : name.to_s.shellescape
     end
@@ -214,7 +225,12 @@ module Tmuxinator
     end
 
     def base_index
-      get_base_index.to_i
+      base = 0
+      if in_current_session
+        # Get the last window index + 1
+        base = 1 + `tmux list-windows  | awk '{print $1}'`.split.last.to_i
+      end
+      base + get_base_index.to_i
     end
 
     def pane_base_index
