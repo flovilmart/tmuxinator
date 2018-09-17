087545fce028988566efbfd7becb4e4c2a91b159
Florent Vilmart <florent@flovilmart.com>
2018-09-16 21:07:33 -0400

    nits

--- lib/tmuxinator/config.rb
+++ lib/tmuxinator/config.rb
@@ -48,9 +48,9 @@ module Tmuxinator
         "#{directory}/default.yml"
       end
 
-      def options 
-        unless exists?(path: options_file) 
-          return 
+      def options
+        unless exists?(path: options_file)
+          return
         end
         YAML.safe_load(File.read(options_file), [], [], true)
       end
